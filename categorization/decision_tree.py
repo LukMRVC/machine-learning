25ca7db36f5f332f91cb081aa97df23907a2ec40
Lukas Moravec <sydnexpklover@gmail.com>
2022-11-05 20:29:31 +0100

    Decision tree fitting

--- categorization/decision_tree.py
+++ categorization/decision_tree.py
@@ -3,36 +3,51 @@ import sys
 import numpy as np
 
 
-class Node:
-    def __init__(self):
+class Tree:
+    def __init__(self, depth=0):
         self.left = None
         self.right = None
-        self.excluded = set()
         self.threshold = -1
-        self.split_arg = -1
+        self.split_attr = -1
+        self.depth = depth
+        self.gini = -1
+        self.values = None
+        self.gini_gain = None
 
+    def fit(self, ds, max_depth):
+        self.values = ds
+        self.left = None
+        self.right = None
+        # split classes from dataset
+        classes = ds[:, -1].astype(dtype=int)
+        ds_without_classes = ds[:, :-1]
+        self.gini_gain = round(gini_impurity(ds, classes), 3)
+        if self.gini_gain <= 0 or self.depth > max_depth:
+            return
+        # call gini
+        gini, threshold, attr_idx = best_gini_split(ds_without_classes, classes)
+        self.threshold = threshold
+        self.split_attr = attr_idx
+        self.gini = gini
+        # make this tree a leaf node if perfect gini split
 
-class Tree:
-    def __init__(self):
-        self.root = None
+        self.left = Tree(self.depth + 1)
+        self.left.fit(ds[ds[:, self.split_attr] <= self.threshold], max_depth)
 
-    def fit(self, ds):
-        dim = len(ds.T)
-        for attr_values in ds.T:
-            mi = min(attr_values)
-            ma = max(attr_values)
-            # print(attr_values)
-            for thold in np.arange(mi, ma, 0.1):
-                satisfied = attr_values < thold
-                print(thold, ": ", satisfied.sum())
-                prob = satisfied.sum()
-                prob *= prob
-                result = 1 - prob
+        self.right = Tree(self.depth + 1)
+        self.right.fit(ds[ds[:, self.split_attr] > self.threshold], max_depth)
 
         pass
 
     def validate():
         pass
+
+
+def gini_impurity(ds, classes):
+    class_set = set(classes)
+    counts = {cs: (ds[:, -1] == cs).sum() for cs in class_set}
+    probs = {cs: probability(count, len(classes)) for cs, count in counts.items()}
+    return 1.0 - sum(probs.values())
 
 
 def probability(count, total):
@@ -73,31 +88,38 @@ def gini_split(values, classes, split):
     return gini_impurity
 
 
-def gini(ds, classes, excluded=[]):
+def best_gini_split(ds, classes):
     best_attr_split = []
-    for attr_idx, attr_values in enumerate(ds.T[:-1]):
-        if attr_idx in excluded:
-            continue
+    for attr_idx, attr_values in enumerate(ds.T):
         mi = min(attr_values)
         ma = max(attr_values)
-        mi += 0.05
-        ma -= 0.05
+        # mi += 0.05
+        # ma -= 0.05
         # print(mi, "\t", ma)
         gini_values = []
 
-        for thold in np.arange(mi, ma, 0.1):
+        for thold in np.arange(mi, ma, 0.05):
             gini_value = gini_split(attr_values, classes, thold)
-            gini_values.append((round(gini_value, 2), round(thold, 2)))
+            gini_values.append((round(gini_value, 4), round(thold, 4)))
+
+        # print(attr_idx, gini_values)
+        # the attributes values are so close together
+        if not gini_values:
+            # append gini for Minimum value and minimum value
+            gini_values.append((gini_split(attr_values, classes, mi), mi))
 
         min_gini, min_thold = min(gini_values)
         best_attr_split.append((min_gini, min_thold, attr_idx))
-        print("Min: ", min(gini_values), attr_idx)
 
     print(best_attr_split)
     min_gini, min_thold, best_split = min(best_attr_split)
-    print(f"Best split: {min_gini} threshold: {min_thold} on attribute {best_split}")
 
-    # print("Impurity: ", impurity)
+    for g, t, s in best_attr_split:
+        if g == min_gini:
+            min_gini, min_thold, best_split = g, t, s
+            break
+
+    return min_gini, min_thold, best_split
 
 
 if __name__ == "__main__":
@@ -110,11 +132,9 @@ if __name__ == "__main__":
             if row:
                 ds.append(row)
     ds = np.array(ds, dtype=float)
-    # split classes from dataset
-    classes = ds[:, -1].astype(dtype=int)
-    ds = ds[:, :-1]
-    # call gini
-    gini(ds, classes)
+    root = Tree()
+    root.fit(ds, 4)
+
     # print(ds.T)
 
     # tr = Tree()
