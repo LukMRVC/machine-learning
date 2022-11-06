4c752d87bca6ef513a006acf4f7804c07fcba8f8
Lukas Moravec <sydnexpklover@gmail.com>
2022-11-06 08:57:53 +0100

    Chooseable class atrribute

--- categorization/decision_tree.py
+++ categorization/decision_tree.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import numpy as np
+import click
 
 
 class Tree:
@@ -15,29 +16,41 @@ class Tree:
         self.gini_gain = None
         self.majority_class = None
 
-    def fit(self, ds, max_depth):
+    def fit(self, ds, max_depth, class_attribute=-1):
         self.values = ds
         self.left = None
         self.right = None
         # split classes from dataset
-        classes = ds[:, -1].astype(dtype=int)
-        ds_without_classes = ds[:, :-1]
-        self.gini_gain, classes_counts = gini_impurity(ds, classes)
+        classes = ds[:, class_attribute].astype(dtype=int)
+
+        self.gini_gain, classes_counts = gini_impurity(ds, classes, class_attribute)
         self.majority_class = max(classes_counts, key=classes_counts.get)
-        if self.gini_gain <= 0 or self.depth > max_depth:
-            return
+
+        # for numpy slicing
+        slic = [True] * len(ds[0])
+        slic[class_attribute] = False
+        ds_without_classes = ds[:, slic]
         # call gini
         gini, threshold, attr_idx = best_gini_split(ds_without_classes, classes)
+        # move attr_index since class attribute column is filtered out from original dataset
+        if attr_idx >= class_attribute:
+            attr_idx += 1
+
         self.threshold = threshold
         self.split_attr = attr_idx
         self.gini = gini
         # make this tree a leaf node if perfect gini split
-
+        if self.gini_gain <= 0 or self.depth >= max_depth:
+            return
         self.left = Tree(self.depth + 1)
-        self.left.fit(ds[ds[:, self.split_attr] <= self.threshold], max_depth)
+        self.left.fit(
+            ds[ds[:, self.split_attr] <= self.threshold], max_depth, class_attribute
+        )
 
         self.right = Tree(self.depth + 1)
-        self.right.fit(ds[ds[:, self.split_attr] > self.threshold], max_depth)
+        self.right.fit(
+            ds[ds[:, self.split_attr] > self.threshold], max_depth, class_attribute
+        )
 
         pass
 
@@ -52,9 +65,9 @@ class Tree:
         return self.majority_class
 
 
-def gini_impurity(ds, classes):
+def gini_impurity(ds, classes, class_attribute):
     class_set = set(classes)
-    counts = {cs: (ds[:, -1] == cs).sum() for cs in class_set}
+    counts = {cs: (ds[:, class_attribute] == cs).sum() for cs in class_set}
     probs = {cs: probability(count, len(classes)) for cs, count in counts.items()}
     return round(1.0 - sum(probs.values()), 3), counts
 
@@ -126,23 +139,34 @@ def best_gini_split(ds, classes):
     return min_gini, min_thold, best_split
 
 
-if __name__ == "__main__":
-    finput = sys.argv[1]
+@click.command()
+@click.argument("datasetfile", type=click.File("r"))
+@click.option("-d", "--delimiter", default=",")
+@click.option(
+    "-h",
+    "--header",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="CSV has header",
+)
+@click.option("-c", "--cls", default=-1, type=int, help="Class index attribute")
+@click.option("--depth", default=5, type=int, help="Maximum tree depth")
+def cli(datasetfile, delimiter, header, cls, depth):
+    reader = csv.reader(datasetfile, delimiter=delimiter)
+    if header:
+        # read header
+        reader.__next__()
     ds = []
+    for row in reader:
+        if row:
+            ds.append(row)
 
-    with open(finput) as f:
-        reader = csv.reader(f, delimiter=";")
-        for row in reader:
-            if row:
-                ds.append(row)
     ds = np.array(ds, dtype=float)
     root = Tree()
-    root.fit(ds, 4)
+    click.echo(f"Training tree with max depth: {depth}")
+    root.fit(ds, depth, cls)
 
-    definetely_some_class = [6.35, 2.7, 4.8, 1.75]
-    predicted = root.predict(definetely_some_class)
-    print(f"Predicted Iris: {predicted}")
-    # print(ds.T)
 
-    # tr = Tree()
-    # tr.fit(ds)
+if __name__ == "__main__":
+    cli()
